882a31bcff3d4e03a5ee643201f6ec6d532a77f8
Dexirius Dev <dev@dexirius.finance>
2021-03-18 17:15:56 -0600

    change lp address

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xF2fc83BcB67e6275f7732FEc60CfDF16E1F9336b',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xa50093755F66B8BB4422821B51630B76C67F9540'
+  lpAddress: '0xbcef69Abc8d92f2bD31Acc94B7e8B7b82027a800'
 }
 
 export default priceContracts
