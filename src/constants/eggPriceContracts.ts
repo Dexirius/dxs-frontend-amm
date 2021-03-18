20c27eafaa5886993ff423a8336077a941706e3d
Dexirius Dev <dev@dexirius.finance>
2021-03-18 16:32:38 -0600

    finish

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x35d81fe74D0CFC375Deba9aD093736C536794dF7',
+  cakeAddress: '0xF2fc83BcB67e6275f7732FEc60CfDF16E1F9336b',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0xa50093755F66B8BB4422821B51630B76C67F9540'
 }
